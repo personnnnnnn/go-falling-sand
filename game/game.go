5ec66e1b4361c17a4004d152b3d90dc41a6ae6ac
Alexandru Busuioc (Alex) <busuioc.alexandru20@gmail.com>
2025-04-05 20:58:07 +0300

    Fixed 'StringToColor'

--- game/game.go
+++ game/game.go
@@ -2,28 +2,57 @@ package game
 
 import (
 	"errors"
-	"github.com/hajimehoshi/ebiten/v2"
+	"fmt"
 	"image/color"
+	"strconv"
+
+	"github.com/hajimehoshi/ebiten/v2"
 )
 
 var Dimensions = struct {
 	Width, Height, ScreenWidth, ScreenHeight int
 }{400, 400, 600, 600}
 
+// HexStringToColor converts a hex color string (e.g., "#1A2B3C") to a color.Color.
+func HexStringToColor(s string) (color.Color, error) {
+	if len(s) != 7 || s[0] != '#' {
+		return color.Black, errors.New("invalid hex color")
+	}
+
+	rStr := s[1:3]
+	gStr := s[3:5]
+	bStr := s[5:7]
+
+	r, err := strconv.ParseUint(rStr, 16, 8)
+	if err != nil {
+		return color.Black, fmt.Errorf("invalid red component: %v", err)
+	}
+	g, err := strconv.ParseUint(gStr, 16, 8)
+	if err != nil {
+		return color.Black, fmt.Errorf("invalid green component: %v", err)
+	}
+	b, err := strconv.ParseUint(bStr, 16, 8)
+	if err != nil {
+		return color.Black, fmt.Errorf("invalid blue component: %v", err)
+	}
+
+	return color.RGBA{uint8(r), uint8(g), uint8(b), 255}, nil
+}
+
 func StringToColor(s string) (color.Color, error) {
 	if len(s) == 0 {
 		return color.Black, errors.New("can't give an empty string")
 	}
 
-	if s[0] != '#' {
-		return color.Black, errors.New("TODO: emplement this")
+	if s[0] == '#' {
+		return HexStringToColor(s)
 	}
 
 	if col, ok := ColorMap[s]; ok {
 		return col, nil
 	}
 
-	return color.Black, errors.New("there is no color '" + s + "'")
+	return color.Black, fmt.Errorf("invalid color name: '%v'", s)
 }
 
 type ElementData struct {
